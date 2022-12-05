231c20f8a164ca9cfda8fe37d5d632cb8337d3fb
Loris Bognanni <loris@codemade.co.uk>
2022-12-05 20:50:46 +0000

    :star: Day 2 part 2

--- aoc/day02/part2.py
+++ aoc/day02/part2.py
@@ -1,4 +1,35 @@
 # Advent of Code - Day 2 - Part Two
 
+values = {
+    "R": 1,
+    "P": 2,
+    "S": 3
+}
+
+strategy = {
+    "X": {
+        "A": "S",
+        "B": "R",
+        "C": "P",
+        "Score": 0
+    },
+    "Y": {
+        "A": "R",
+        "B": "P",
+        "C": "S",
+        "Score": 3
+    },
+    "Z": {
+        "A": "P",
+        "B": "S",
+        "C": "R",
+        "Score": 6
+    }
+}
+
 def result(input):
-    return None
+    score = 0
+    for l in input:
+        hands = l.split(" ")
+        score += strategy[hands[1]]["Score"] + values[strategy[hands[1]][hands[0]]]
+    return score
