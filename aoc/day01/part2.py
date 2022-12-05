dfc355a2e582f1e62b20808b7622caa24e071b94
Loris Bognanni <loris@codemade.co.uk>
2022-12-05 20:19:21 +0000

    :star: Day 1 part 2

--- aoc/day01/part2.py
+++ aoc/day01/part2.py
@@ -1,4 +1,18 @@
 # Advent of Code - Day 1 - Part Two
 
+def topThree(arr):
+    return sorted(arr, reverse=True)[0:3]
+
+
 def result(input):
-    return input
+    elfs = []
+    current = 0
+    for l in input:
+        if l == "":
+            elfs.append(current)
+            current = 0
+        else:
+            current = current + int(l)
+    if (current > 0):
+        elfs.append(current)
+    return sum(topThree(elfs))
