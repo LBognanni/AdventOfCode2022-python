868e32e2a42f3deb378dcc1a2253b5a0c14efab1
Loris Bognanni <loris@codemade.co.uk>
2022-12-05 20:13:07 +0000

    :star: Day01 part 1

--- aoc/day01/part1.py
+++ aoc/day01/part1.py
@@ -1,4 +1,14 @@
 # Advent of Code - Day 1 - Part One
 
 def result(input):
-    return input
+    elfs = []
+    current = 0
+    for l in input:
+        if l == "":
+            elfs.append(current)
+            current = 0
+        else:
+            current = current + int(l)
+    if (current > 0):
+        elfs.append(current)
+    return max(elfs)
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -4,12 +4,28 @@ from aoc.day01 import part1, part2
 # --- Part One ---
 #
 
+test_input = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
 def test_part1():
-    assert part1.result(None) == None
+    maxCalories = part1.result(test_input.split("\n"))
+    assert maxCalories == 24000
 
 #
 # --- Part Two ---
 #
 
 def test_part2():
-    assert part2.result(None) == None
+    assert part2.result(test_input.split("\n")) == 45000
