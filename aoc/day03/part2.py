9de4d83db194cebe22dfe6dff80fb44d7f4b7ff9
Loris Bognanni <loris@codemade.co.uk>
2022-12-05 22:32:02 +0000

    :star: Day 3 part 2

--- aoc/day03/part2.py
+++ aoc/day03/part2.py
@@ -1,4 +1,14 @@
 # Advent of Code - Day 3 - Part Two
+from aoc.day03 import part1
 
-def result(input):
-    return input
+def chunk_list (list, x):
+    return [list[i:i+x] for i in range(0, len(list), x)]
+
+def find_shared(list1, list2, list3):
+    for c in list1:
+        if c in list2 and c in list3:
+            return c
+    return None
+
+def result(input:list[str]):
+    return sum(list(map(lambda group: part1.score(find_shared(group[0], group[1], group[2])), chunk_list(input, 3))))
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -19,4 +19,5 @@ def test_part1():
 #
 
 def test_part2():
-    assert part2.result(None) == None
+    
+    assert part2.result(test_input) == 70
