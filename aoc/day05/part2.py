fd0708c0ad9f71097843bd1b4b57bf9a0a2db93e
Loris Bognanni <loris@codemade.co.uk>
2022-12-06 20:49:03 +0000

    :star: Day 5 part 2

--- aoc/day05/part2.py
+++ aoc/day05/part2.py
@@ -1,4 +1,12 @@
 # Advent of Code - Day 5 - Part Two
+from aoc.day05 import part1
 
 def result(input):
-    return None
+    (stacks, movements) = part1.parse(input)
+
+    for (howMany, fromPos, toPos) in movements:
+        els = stacks[fromPos][:howMany]
+        stacks[toPos] = els + stacks[toPos]
+        stacks[fromPos] = stacks[fromPos][howMany:]
+
+    return "".join(map(lambda x:x[0], stacks.values()))
